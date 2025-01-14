cc1e4a60a814174b03e196ac9456cad517eec610
agmgomes <agmgomes94@gmail.com>
2025-01-14 18:17:24 +0000

    removed sendgrid adapter

--- src/main/java/com/agmgomes/emailservice/infrastructure/adapters/config/BeanConfig.java
+++ src/main/java/com/agmgomes/emailservice/infrastructure/adapters/config/BeanConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Configuration;
 
 import com.agmgomes.emailservice.application.ports.out.EmailSenderPort;
 import com.agmgomes.emailservice.infrastructure.adapters.out.aws.AwsSesAdapter;
-import com.agmgomes.emailservice.infrastructure.adapters.out.sendgrid.SendGridAdapter;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,24 +15,12 @@ import lombok.extern.slf4j.Slf4j;
 @Configuration
 public class BeanConfig {
 
-    @Value("${email.provider}")
-    private String emailProvider;
-
-    private final SendGridAdapter sendGridAdapter;
     private final AwsSesAdapter awsSesAdapter;
 
     @Bean
     public EmailSenderPort emailSenderPort() {
-        if("sendgrid".equals(emailProvider)) {
-            log.info("Email Provider Port: SendGrid Adapter.");
-            return sendGridAdapter;
-        } else if("aws".equals(emailProvider)) {
-            log.info("Email Provider Port: AWS SES Adapter.");
-            return awsSesAdapter;
-        }
-        else {
-            throw new IllegalArgumentException("Provider not supported");
-        }
+        log.info("Email Sender Port: AWS SES Adapter.");
+        return awsSesAdapter;
     }
 
 }
